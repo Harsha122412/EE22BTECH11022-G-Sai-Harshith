5c90bdfe240dbbd3689f2ec74bfb7c53b8b11d69
Harsha122412 <139646786+Harsha122412@users.noreply.github.com>
2023-10-24 01:17:18 +0530

    Delete MusicPlayer directory

--- MusicPlayer/MusicPlayer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path  # make sure to import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('musicapp.urls')),  # include the app urls
-]

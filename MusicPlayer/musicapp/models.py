59b02edf16343378636aa180f1d4a6c78c9d2ea8
Harsha122412 <139646786+Harsha122412@users.noreply.github.com>
2023-11-03 22:28:18 +0530

    Delete MusicPlayer directory

--- MusicPlayer/musicapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Song(models.Model):
-    title = models.CharField(max_length=100)
-    audio_file = models.FileField(upload_to='songs')
-    path = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
